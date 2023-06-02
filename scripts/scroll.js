57a760bae80edc2458fd429fa07060e7caa3fe00
Felipe Gonçalves De Souza <lipesouzago4@gmail.com>
2023-06-02 11:00:20 -0400

    Atualização de problemas
    Imagem trocada, ajuste em  tela rolando para o lado, link do perfil do linkedln e animações

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -2,18 +2,35 @@ const target = document.querySelectorAll('[data-anime]')
 
 const animationClass = "animate"
 
-function anime(){
-const windowTop =  window.pageYOffset + window.innerHeight - 80
-target.forEach(function(element){
- if((windowTop) > element.offsetTop){
-    element.classList.add(animationClass)
- }
-else{
-    element.classList.remove(animationClass)
-}
-})
+
+function anime() {
+   
+// CÓDIGO BENZIDO, FAVOR NÃO MEXER
+    if (body.style.width > 400) {
+        var windowTop = window.pageYOffset + window.innerHeight - 800
+
+    } else if (body.style.width <= 400) {
+        var windowTop = window.pageYOffset + window.innerHeight - 50
+    }
+
+    target.forEach(function (element) {
+        if (window.pageYOffset > 100) {
+            if ((windowTop) > element.offsetTop) {
+                element.classList.add(animationClass)
+
+            } else {
+                element.classList.remove(animationClass)
+
+            }
+
+        } else if(window.pageYOffset + window.innerHeight -50  < 1568   ) {
+            element.classList.add(animationClass)
+
+        }
+
+    })
 }
 
-window.addEventListener('scroll',function(){
+window.addEventListener('scroll', function () {
     anime()
 })
